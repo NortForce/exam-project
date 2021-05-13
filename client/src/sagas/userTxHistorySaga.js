4c2b17c9f0bfc3ae2803f8330624aee429fd1680
Kyrylo Rozumovskyi <nortforce@gmail.com>
2021-05-13 20:37:41 +0300

    Refactor saga

--- client/src/sagas/userTxHistorySaga.js
+++ client/src/sagas/userTxHistorySaga.js
@@ -5,8 +5,8 @@ import * as restController from 'api/http/restController';
 export function * getUserTxHistorySaga (action) {
   try {
 
-    const {data: {data: {userTransactionHistory, aggregatedMoneyMovement}}} = yield restController.getUserTxHistory(action.payload);
-    yield put(ACTION.getUserTxHistorySuccess({userTransactionHistory, aggregatedMoneyMovement}));
+    const {data: {data}} = yield restController.getUserTxHistory(action.payload);
+    yield put(ACTION.getUserTxHistorySuccess(data));
 
   } catch (error) {
     yield put(ACTION.getUserTxHistoryError(error))
